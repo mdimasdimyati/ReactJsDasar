674c0c37c3a0821c7eaa882f705d74b88555afaf
mdimasdimyati <androdim888@gmail.com>
2021-09-08 17:00:37 +0700

    Add Form

--- src/App.js
+++ src/App.js
@@ -1,49 +1,65 @@
 import React, {Component} from 'react'
 import Table from './Table'
+import Formulir from './Formulir'
 
 
 
 class App extends Component {
-  removeCharacter = (index) => {
-    const {characters} = this.state
-  
-    this.setState({
-      characters: characters.filter((character, i) => {
-        return i !== index
-      }),
-    })
-  }
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ],
+    characters: [],
   }
-
-  render() {
+  render () {
     const { characters } = this.state
-
     return (
       <div className="container">
-      <Table characterData={characters} removeCharacter={this.removeCharacter} />
-    </div>
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Formulir />
+      </div>
     )
   }
 }
+
+// class App extends Component {
+//   removeCharacter = (index) => {
+//     const {characters} = this.state
+  
+//     this.setState({
+//       characters: characters.filter((character, i) => {
+//         return i !== index
+//       }),
+//     })
+//   }
+//   state = {
+//     characters: [
+//       {
+//         name: 'Charlie',
+//         job: 'Janitor',
+//       },
+//       {
+//         name: 'Mac',
+//         job: 'Bouncer',
+//       },
+//       {
+//         name: 'Dee',
+//         job: 'Aspring actress',
+//       },
+//       {
+//         name: 'Dennis',
+//         job: 'Bartender',
+//       },
+//     ],
+//   }
+
+//   render() {
+//     const { characters } = this.state
+
+//     return (
+//       <div className="container">
+//       <Table characterData={characters} removeCharacter={this.removeCharacter} />
+//     </div>
+//     )
+//   }
+// }
 
 
 export default App;
